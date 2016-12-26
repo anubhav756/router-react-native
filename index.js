b9a2223a3163e32629f553863f707eacb434e5f1
Brijesh Pant <brijesh.pant@daffodilsw.com>
2016-12-26 18:00:28 +0530

    initial route only name required

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ export default class App extends Component {
 	render() {
 		return (
 			<Navigator
-				initialRoute={this.props.initialRoute}
+                initialRoute={this.props.routes[this.props.initialRoute]}
 				renderScene={renderScene}
 				navigationBar={<HomeLayout navBar={this.props.navBar} leftPanel={this.props.leftPanel} onOpen={this.props.onOpen} onClose={this.props.onClose} leftPanelOffset={this.props.leftPanelOffset} />}
 				/>
